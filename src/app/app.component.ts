c8e755c99f4ddb29373b745b0b7f65dec4fa1891
pratiga <gaitrisanu2468@gmail.com>
2022-08-07 23:38:22 +0545

    add slider
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 import { policy } from './policy';
 import { PolicyService } from './policy.service';
 import { seller } from './bestSeller';
@@ -13,6 +14,23 @@ export class AppComponent implements OnInit {
   policy: any;
   sellers: seller[] =[];
   seller:any;
+  partnersArray:any = [
+    {
+      imgName: "../assets/policy/p1.png"
+    },
+    {
+      imgName: "../assets/policy/p2.png"
+    }, {
+      imgName: "../assets/policy/p3.png"
+    }, {
+      imgName: "../assets/policy/pp2.png"
+    }, {
+      imgName: "../assets/policy/pp4.jpg"
+    }, {
+      imgName: "../assets/policy/pp5.jpg"
+    },
+  ]
+  
  
   
   constructor(private ps : PolicyService){}
@@ -27,6 +45,56 @@ export class AppComponent implements OnInit {
     changeImage(event:any){
         this.url = event.target.src;
         console.log();
+    }
+    
+    customOptions: OwlOptions = {
+      loop: true,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: true,
+      dots: false,
+      navSpeed: 700,
+      navText: ['', ''],
+      responsive: {
+        0: {
+          items: 1
+        },
+        400: {
+          items: 2
+        },
+        740: {
+          items: 3
+        },
+        940: {
+          items: 6
+        }
+      },
+      nav: false
+    }
+    
+    policyOptions: OwlOptions = {
+      loop: true,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: true,
+      dots: false,
+      navSpeed: 700,
+      navText: ['<i class="fa fa-caret-right"></i>', '<i class="fa fa-caret-left"></i>'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        400: {
+          items: 2
+        },
+        740: {
+          items: 3
+        },
+        940: {
+          items: 3
+        }
+      },
+      nav: true
     }
   
   }
