85ee0d66adb30d753470a09566bf8b72733eda1a
pratiga <gaitrisanu2468@gmail.com>
2022-08-07 07:51:38 +0545

    add policy content
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { policy } from './policy';
+import { PolicyService } from './policy.service';
+import { seller } from './bestSeller';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'website';
+export class AppComponent implements OnInit {
+  policys: policy[] = [];
+  policy: any;
+  sellers: seller[] =[];
+  seller:any;
+ 
   
-  constructor(){}
-  ngOnInit(){}
+  constructor(private ps : PolicyService){}
+  ngOnInit(): void {
+    this.policys = this.ps.getAll();
+    this.sellers = this.ps.getseller();
+    
+  }
     
   
     url:string = "../assets/img1.jpg";
