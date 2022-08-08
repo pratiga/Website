31721eb84f7cabbc42a4fdf8de5e61c4237f6097
pratiga <gaitrisanu2468@gmail.com>
2022-08-08 22:28:01 +0545

    add footer data
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/policy.service.ts
+++ src/app/policy.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { policy } from './policy';
 import { seller } from './bestSeller';
+import { footer } from './footer';
 @Injectable({
   providedIn: 'root'
 })
@@ -26,6 +27,38 @@ export class PolicyService {
         image1:'./assets/image1.jpg',
         image2:'./assets/img4.jpg',
         image3:'./assets/img3.jpg',
+      },
+      
+    ]
+  }
+  getfooter():footer[]{
+    return[
+      {
+        id:1,
+        title:'my account',
+        c1:'information',
+        c2:'order history',
+        c3:'my wishlist',
+        c4:'privacy policy',
+        c5:'site map',
+      },
+      {
+        id:2,
+        title:'information',
+        c1:'cart',
+        c2:'product support',
+        c3:'pc setup and support',
+        c4:'services',
+        c5:'extended services plans',
+      },
+      {
+        id:3,
+        title:'our services',
+        c1:'about',
+        c2:'shipping guide',
+        c3:'store location',
+        c4:'aifilates',
+        c5:'customer point policy',
       }
     ]
   }
@@ -51,6 +84,14 @@ export class PolicyService {
         id:3,
         title:'this is title',
         imageUrl:'./assets/img5.jpg',
+        date_days: 18,
+        date_month: "may",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla necessitatibus mollitia facilis numquam nesciunt odit, voluptatum quae incidunt tempora praesentium deserunt velit? Recusandae in cumque amet optio tempora repellendus atque?",
+      },
+      {
+        id:3,
+        title:'this is title',
+        imageUrl:'./assets/image1.jpg',
         date_days: 18,
         date_month: "may",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla necessitatibus mollitia facilis numquam nesciunt odit, voluptatum quae incidunt tempora praesentium deserunt velit? Recusandae in cumque amet optio tempora repellendus atque?",
